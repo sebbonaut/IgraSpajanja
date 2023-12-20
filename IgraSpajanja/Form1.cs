8446d8e6fd69d89e5f69189264bf0cab89488694
sebas <sebas@DESKTOP-S92RB8H>
2023-12-20 15:40:04 +0100

    Add project files.

---
+++ IgraSpajanja/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IgraSpajanja
+{
+    public partial class Form1 : Form
+    {
+        public static int vrijeme = 700;
+        Random random = new Random();
+        Label prvaOdabrana = null, drugaOdabrana = null;
+        /// <summary>
+        /// Svaku ikonu iz liste stavimo kao tekst nekoj labeli.
+        /// </summary>
+        private void RaspodijeliIkone()
+        {
+            List<string> ikone = new List<string>()
+            {
+                "c", "c", "!", "!", "N", "N", "b", "b",
+                "v", "v", "w", "w", "z", "z", ",", ","
+            };
+            tableLayoutPanel1.SuspendLayout();
+            foreach (Control kontrola in tableLayoutPanel1.Controls)
+            {
+                if (kontrola is Label labela)
+                {
+                    int randomBroj = random.Next(ikone.Count);
+                    labela.Text = ikone[randomBroj];
+                    labela.ForeColor = labela.BackColor;
+                    ikone.RemoveAt(randomBroj);
+                }
+            }
+            tableLayoutPanel1.ResumeLayout(false);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            RaspodijeliIkone();
+        }
+
+        private void ProvjeriPobjedu()
+        {
+            foreach (Control kontrola in tableLayoutPanel1.Controls)
+                if (kontrola is Label labela && labela.ForeColor
+                    == labela.BackColor)
+                    return;
+            if (MessageBox.Show("Želite li opet igrati?", "Čestitamo!",
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    RaspodijeliIkone();
+            else
+                Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            prvaOdabrana.ForeColor = prvaOdabrana.BackColor;
+            drugaOdabrana.ForeColor = drugaOdabrana.BackColor;
+            prvaOdabrana = drugaOdabrana = null;
+        }
+
+        private void uputeZaIgruToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pronađite sve parove ikona!",
+                "Cilje igre");
+        }
+
+        private void postavkeIgreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        void Oboji(Color boja)
+        {
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel1.BackColor = boja;
+            foreach (Control kontrola in tableLayoutPanel1.Controls)
+            {
+                if(kontrola is Label labela)
+                {
+                    labela.BackColor = boja;
+                    if(labela.ForeColor != Color.Black)
+                    {
+                        labela.ForeColor = labela.BackColor;
+                    }
+                }
+            }
+            tableLayoutPanel1.ResumeLayout(false);
+        }
+        private void odabirBojeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (colorDialog1.Color == Color.Black)
+                    MessageBox.Show("Izaberite neku drugu!");
+                else
+                    Oboji(colorDialog1.Color);
+            }
+        }
+
+        private void promijeniVrijemeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        /// <summary>
+        /// Kod za obradu svakog događaja klika na neku labelu.
+        /// </summary>
+        /// <param name="sender">Labela koja je kliknuta.</param>
+        /// <param name="e"></param>
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (sender is Label labela && timer1.Enabled == false)
+            {
+                if (labela.ForeColor == labela.BackColor)
+                {
+                    if (prvaOdabrana == null)
+                    {
+                        prvaOdabrana = labela;
+                        labela.ForeColor = Color.Black;
+                    }
+                    else
+                    {
+                        drugaOdabrana = labela;
+                        drugaOdabrana.ForeColor = Color.Black;
+                        ProvjeriPobjedu();
+                        if (prvaOdabrana.Text == drugaOdabrana.Text)
+                            prvaOdabrana = drugaOdabrana = null;
+                        else
+                        {
+                            timer1.Interval = vrijeme;
+                            timer1.Start();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IgraSpajanja/Form2.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace IgraSpajanja
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Odaberite vrijeme:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Items.AddRange(new object[] {
+            "400",
+            "700",
+            "1000"});
+            this.listBox1.Location = new System.Drawing.Point(12, 52);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(244, 128);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(87, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 251);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Odabir vremena";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
